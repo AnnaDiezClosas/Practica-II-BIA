2bff2198c57c72fc703c5b9ea19c1e409f951c77
evavivesm <evavivesm@iqs.edu>
2020-06-02 14:33:31 +0200

    hemos quitado columna jurisdiction annual report

---
+++ CODIGO.R
@@ -0,0 +1,11 @@
+#proyecto
+
+
+
+dfIndicators <- read.table("OECD-ADIMA-Indicators.txt", sep="\t", dec=".", quote = "\"'",
+                      header=TRUE, skip = 1, na.strings = "NA")
+str(dfIndicators)
+dfIndicators1 <- dfIndicators
+dfIndicators1 <- dfIndicators1[,-5]
+dfIndicators
+dfIndicators1
