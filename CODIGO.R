53081f515b136c6578922f21b1f0d2bf8d4f632c
evavivesm <evavivesm@iqs.edu>
2020-06-02 15:35:43 +0200

    prueba 1213232

--- CODIGO.R
+++ CODIGO.R
@@ -1,1 +1,9 @@
 #proyecto
+
+dfIndicators <- read.table("OECD-ADIMA-Indicators.txt", sep="\t", dec=".", quote = "\"'",
+                           header=TRUE, skip = 1, na.strings = "NA")
+str(dfIndicators)
+dfIndicators1 <- dfIndicators
+dfIndicators1 <- dfIndicators1[,-5]
+dfIndicators
+dfIndicators1
