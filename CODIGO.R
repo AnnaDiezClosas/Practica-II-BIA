40c1203e36a2e6c096409ba1e1541c5773ef9e95
Anna Diez Closas <adiezclosas@gmail.com>
2020-06-03 21:09:28 +0200

    txt arreglado + dataframe de empresas a mirar su tendencia

--- CODIGO.R
+++ CODIGO.R
@@ -1,7 +1,7 @@
 #proyecto
 
 dfIndicators <- read.table("OECD-ADIMA-Indicators.txt", sep="\t", dec=".", quote = "\"'",
-                           header=TRUE, skip = 1, na.strings = "NA")
+                           header=TRUE, skip = 0, na.strings = "NA")
 
 str(dfIndicators)
 if(!require("ggplot2")) {
@@ -14,8 +14,7 @@ if(!require("ggplot2")) {
 ggplot(dfIndicators, aes(x=Headquarters.of.Parent.MNE)) + geom_bar()
 
 #¿Dónde pagan impuestos?
-
-
+#Cambiar Not Found por NA
 
 
 
@@ -23,7 +22,7 @@ ggplot(dfIndicators, aes(x=Headquarters.of.Parent.MNE)) + geom_bar()
 #¿Cómo ha afectado COVID a las multinacionales?
 
 dfTrends <- read.table("OECD-ADIMA-500-Google-trends-monitor.txt", sep="\t", dec=".", quote = "\"'",
-                           header=TRUE, skip = 1, na.strings = "NA")
+                           header=TRUE, skip = 0, na.strings = "NA")
 
-dfTrends1 <- dfTrends[c(16,25,38,56,167,177,201,204,186),] #ahora todas filas y solo columnas 1,3
+dfTrends1 <- dfTrends[c(16,25,38,56,167,177,201,204,186,440),] #ahora todas filas y solo columnas de las empresas que queremos analizar
 dfTest2
