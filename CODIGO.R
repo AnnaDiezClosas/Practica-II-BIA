e9bb4ee42c7783b75f52f4f2040a6abf9bb696e5
evavivesm <evavivesm@iqs.edu>
2020-06-04 14:12:13 +0200

    grafico indice airbus

--- CODIGO.R
+++ CODIGO.R
@@ -21,8 +21,23 @@ ggplot(dfIndicators, aes(x=Headquarters.of.Parent.MNE)) + geom_bar()
 
 #¿Cómo ha afectado COVID a las multinacionales?
 
-dfTrends <- read.table(file = "OECD-ADIMA-500-Google-trends-monitor.txt", header = TRUE, sep = "\t", dec =",")
-dfTrends$Parent.MNE <- as.character(dfTrends$Parent.MNE)
+dfTrends <- read.table(file = "OECD-ADIMA-500-Google-trends-monitor.txt", header = FALSE, sep = "\t", dec =",")
+dfTrends$V1 <- as.character(dfTrends$V1)
+
 names(dfTrends)[2:76] <- format(as.Date(names(dfTrends)[2:76], format = "X%d.%m.%Y"), format = "%Y-%m-%d")
 
-dfTrends1 <- dfTrends[c(16,25,38,56,167,177,201,204,186,440),] #ahora todas filas y solo columnas de las empresas que queremos analizar
+dfTrends1 <- dfTrends[c(1,17,26,39,57,168,178,202,205,187,441),] #ahora todas filas y solo columnas de las empresas que queremos analizar
+
+dfTrends1_transpose <- data.frame(t(dfTrends1[-1]))
+colnames(dfTrends1_transpose) <- dfTrends1[, 1]
+
+names (dfTrends1_transpose)[1] = "Date"
+rownames(dfTrends1_transpose)<-1:75
+
+dfTrends1_transpose$`Airbus SE` <- as.numeric(sub(",",".",dfTrends1_transpose$`Airbus SE`,fixed=TRUE))
+dfTrends1_transpose$Date <-as.Date(dfTrends1_transpose$Date, format = "%d/%m/%Y")
+
+f1<-ggplot(dfTrends1_transpose, aes(Date, `Airbus SE` )) + scale_colour_identity()+geom_line(color="blue")+theme_minimal()
+f1
+
+       
